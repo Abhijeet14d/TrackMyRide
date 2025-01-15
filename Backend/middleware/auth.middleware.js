30b1878f05ff77d3dba64aaaa4d028b3b10249bb
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-01-15 22:09:15 +0530

    created user all route

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,12 +1,19 @@
 const userModel = require('../models/user.model');
 const bycrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1] || req.cookies.token;
+    const authHeader = req.headers.authorization;
+    const token = authHeader ? authHeader.split(' ')[1] : req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: 'Access denied' });
     }
+    const isBlackListed = await blacklistTokenModel.findOne({ token: token });
+    if(isBlackListed){
+        return res.status(401).json({message: 'Unauthorized access'});
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await userModel.findById(decoded._id);
